df6f0b785f80258a1b766a44b4c724e8cadcade6
Stuart Enters <senters@crisisprevention.com>
2019-07-01 14:56:39 -0500

    adding in logon functionality

--- public/app.js
+++ public/app.js
@@ -1,11 +1,22 @@
 const CreateUser = document.querySelector('.CreateUser')
+const Login = document.querySelector('.Login')
 
 CreateUser.addEventListener('submit', (e) => {
   e.preventDefault()
   const username = CreateUser.querySelector('.username').value
-    const password = CreateUser.querySelector('.password').value
-    console.log(`creating user with username ${username} and password ${password}`);
+  const password = CreateUser.querySelector('.password').value
+  console.log(`creating user with username ${username} and password ${password}`);
   post('/createUser', { username, password })
+})
+
+Login.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const username = CreateUser.querySelector('.username').value
+    const password = CreateUser.querySelector('.password').value
+    post('/login', { username, password }).then(({ status }) => {
+        if (status === 200) alert('login success')
+        else alert('login failed')
+    })
 })
 
 function post (path, data) {
